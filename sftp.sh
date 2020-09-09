66b5077e120ce9631b00fb9c94962c97d2cc2dfe
Don Filemon <filexpvp@gmail.com>
2020-09-09 14:59:27 +0200

    Add files via upload

---
+++ sftp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+server=0.0.0.0
+port=22
+login=user
+PW='passwd'
+dirto=/home/user
+dirfrom=/home/user
+regex="*.txt"
+recursivelvl=1
+howold="-60"
+findfile=$(find $dirfrom -maxdepth $recursivelvl -name "$regex" -type f -mtime $howold)
+echo $findfile
+for i in $findfile
+          do
+          sshpass -p $PW sftp -P $port $login@$server:$dirto <<< $"put $i"
+          done
